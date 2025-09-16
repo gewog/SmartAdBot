49fe5a8e6e60a1cfb8b6908a7c4d50014a6d7401
gewoggewog <gewoggewog@gmail.com>
2025-09-16 17:35:44 +0300

    add bot handlers and PostgreSQL connection

--- config/config.py
+++ config/config.py
@@ -9,22 +9,30 @@ Attributes:
     bot (Bot): Aiogram Bot instance.
     dp (Dispatcher): Aiogram Dispatcher instance with MemoryStorage for FSM.
 """
+from pathlib import Path
+
 
 from aiogram import Bot, Dispatcher
 from dotenv import dotenv_values
 
 from aiogram.fsm.storage.memory import MemoryStorage
 
+
 # Load environment variables from the .env file
-config: dict = dotenv_values(r"config/.env")
+env_path = Path(__file__).parent / ".env"
+config = dotenv_values(env_path)
+
 
 # Extract the Telegram Bot API token from the config
 API_TOKEN: str = config["TOKEN"]
 
 ADMIN = int(config["ADMIN_ID"])
+SQLALCHEMY_URL = config["SQLALCHEMY_URL"]
 
 # Initialize the Bot instance with the API token
 bot: Bot = Bot(token=API_TOKEN)
 
 # Initialize the Dispatcher with MemoryStorage for FSM
 dp: Dispatcher = Dispatcher(storage=MemoryStorage())
+
+# print("Loaded .env:", config)
